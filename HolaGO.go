6aa4327552fe271b767eba4e33f50993516bdf60
geeka1 <mariasol.causarano@gmail.com>
2016-08-29 19:46:23 -0300

    voleteamos tuti

--- HolaGO.go
+++ HolaGO.go
@@ -4,8 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola GO, voy a a prender de que se tratat todo esto")
-	fmt.Println("Vamos a ver si aparece este cambio")
-	fmt.Println("Vamos a ver si aparece este cambio")
-	fmt.Println("Vamos a ver si aparece este cambio")
-	fmt.Println("Y si hacemos esto???")
 }
