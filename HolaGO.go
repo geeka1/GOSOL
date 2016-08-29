acb063efe92dd62a0801f3268e9c053f60cd2c79
geeka1 <mariasol.causarano@gmail.com>
2016-08-29 19:42:00 -0300

    una linea mas

--- HolaGO.go
+++ HolaGO.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola GO, voy a a prender de que se tratat todo esto")
+	fmt.Println("Vamos a ver si aparece este cambio")
+	fmt.Println("Vamos a ver si aparece este cambio")
+	fmt.Println("Vamos a ver si aparece este cambio")
 }
